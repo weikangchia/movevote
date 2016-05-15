d6311e70416416048149e45e4a83235ed39cbccb
Wei Kang <weikangchia@gmail.com>
2016-05-16 00:30:25 +0800

    add jar files for google identity toolkit

--- src/com/appspot/movevote/controller/HomeServlet.java
+++ src/com/appspot/movevote/controller/HomeServlet.java
@@ -1,5 +1,6 @@
 package com.appspot.movevote.controller;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -8,7 +9,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONException;
+
+import com.appspot.movevote.entity.Constant;
 import com.appspot.movevote.entity.InSingMovie;
+import com.google.identitytoolkit.GitkitClient;
+import com.google.identitytoolkit.GitkitClientException;
+import com.google.identitytoolkit.GitkitUser;
 
 /**
  * Servlet implementation class HomeServlet
@@ -32,8 +39,46 @@ public class HomeServlet extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		ArrayList<InSingMovie> movieList = InSingMovie.retrieveMovieList();
-
 		request.setAttribute("movieList", movieList);
+
+		// check if user is login
+		try {
+			GitkitUser gitkitUser = null;
+			GitkitClient gitkitClient = GitkitClient
+					.newBuilder()
+					.setGoogleClientId(
+							"477425429910-r99e3tmjtvst1qsfp0ttk9gcs9ffbq68.apps.googleusercontent.com")
+					.setProjectId("movevote")
+					.setWidgetUrl(Constant.PUB_HOSTNAME + "gitkit")
+					.setServiceAccountEmail("weikangchia@gmail.com")
+					.setCookieName("gtoken")
+					.setKeyStream(
+							this.getClass().getResourceAsStream(
+									"/assets/secret/movevote-ac049cfdd3e4.p12"))
+					.build();
+
+			gitkitUser = gitkitClient.validateTokenInRequest(request);
+			String userInfo = null;
+			if (gitkitUser != null) {
+				userInfo = "Welcome back!<br><br> Email: "
+						+ gitkitUser.getEmail() + "<br> Id: "
+						+ gitkitUser.getLocalId() + "<br> Provider: "
+						+ gitkitUser.getCurrentProvider();
+			} else {
+				System.out.println("you are not login");
+			}
+			response.setStatus(HttpServletResponse.SC_OK);
+		} catch (GitkitClientException | JSONException e) {
+			e.printStackTrace();
+			System.out.println(HttpServletResponse.SC_NOT_FOUND);
+		}
+
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		response.addHeader("Access-Control-Allow-Headers",
+				"Content-Type, Authorization");
+		response.addHeader("Access-Control-Allow-Methods",
+				"PUT, GET, POST, DELETE, OPTIONS");
+		response.setStatus(HttpServletResponse.SC_OK);
 
 		getServletContext().getRequestDispatcher("/index.jsp").forward(request,
 				response);
--- src/com/appspot/movevote/entity/Constant.java
+++ src/com/appspot/movevote/entity/Constant.java
@@ -2,4 +2,6 @@ package com.appspot.movevote.entity;
 
 public class Constant {
 	public static final String INSING_HOSTNAME = "http://www.insing.com/";
+	public static final String DEV_HOSTNAME = "https://localhost:8888/";
+	public static final String PUB_HOSTNAME = "https://movevote.appspot.com/";
 }
