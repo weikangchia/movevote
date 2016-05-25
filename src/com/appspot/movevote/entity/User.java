6740be23cb45ad04952f994430d1e0fb9bff7cd3
weikang <weikang@192.168.1.5>
2016-05-25 22:32:41 +0800

    update to show empty profile picture if the user do not have any profile image

---
+++ src/com/appspot/movevote/entity/User.java
@@ -0,0 +1,31 @@
+package com.appspot.movevote.entity;
+
+public class User extends Person {
+	private String email;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(String id, String name, String profilePath, String email) {
+		super(id, name, profilePath);
+		if (profilePath == null || profilePath.equals("")) {
+			setProfilePath("/assets/img/profile/empty_profile.png");
+		}
+		this.setEmail(email);
+	}
+
+	public User(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
